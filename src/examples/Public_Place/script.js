195c8371bcd4990e7e4bda806c43c87ae1ec2792
maryamd <93530294+maryamagd@users.noreply.github.com>
2022-03-21 05:46:46 +0300

    EDIT3

--- src/examples/Public_Place/script.js
+++ src/examples/Public_Place/script.js
@@ -9,7 +9,10 @@ const loader = new Rhino3dmLoader()
 loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/' )
 
 // initialise 'data' object that will be used by compute()
-const definition = 'Canopy_maker.gh'
+const data = {
+  definition: 'Canopy_Maker.gh',
+  inputs: getInputs()
+}
 
 // setup input change events
 const Column_Radius_slider = document.getElementById( 'Column_Radius' )
@@ -18,6 +21,15 @@ Column_Radius_slider.addEventListener( 'touchend', onSliderChange, false )
 const Column_Height_slider = document.getElementById( 'Column_Height' )
 Column_Height_slider.addEventListener( 'mouseup', onSliderChange, false )
 Column_Height_slider.addEventListener( 'touchend', onSliderChange, false )
+const U_Divisions_slider = document.getElementById( 'U_Divisions' )
+U_Divisions_slider.addEventListener( 'mouseup', onSliderChange, false )
+U_Divisions_slider.addEventListener( 'touchend', onSliderChange, false )
+const V_Divisions_slider = document.getElementById( 'V_Divisions' )
+V_Divisions_slider.addEventListener( 'mouseup', onSliderChange, false )
+V_Divisions_slider.addEventListener( 'touchend', onSliderChange, false )
+const Canopy_Depth_slider = document.getElementById( 'Canopy_Depth' )
+Canopy_Depth_slider.addEventListener( 'mouseup', onSliderChange, false )
+Canopy_Depth_slider.addEventListener( 'touchend', onSliderChange, false )
 
 let points = []
 
@@ -35,16 +47,20 @@ rhino3dm().then(async m => {
 })
 
 function rndPts() {
-  // generate random points
+  // generate Inital points
+  const startPts = [
+    { x: 1.5, y: 0.3, z: 0 },
+    { x: 2.1, y: 28.9, z: 0 },
+    { x: 33.18, y: 20, z: 0 },
+    { x: 20.54, y: -5.17, z: 0 },
+  ]
 
-  const cntPts = 4
-  const bndX = Column_Radius_slider.valueAsNumber / 2
-  const bndY = Column_Radius_slider.valueAsNumber / 2
+  const cntPts = startPts.length
 
   for (let i = 0; i < cntPts; i++) {
-    const x = Math.random() * (bndX - -bndX) + -bndX
-    const y = Math.random() * (bndY - -bndY) + -bndY
-    const z = 0
+    const x = startPts[i].x
+    const y = startPts[i].y
+    const z = startPts[i].z
 
     const pt = "{\"X\":" + x + ",\"Y\":" + y + ",\"Z\":" + z + "}"
 
@@ -53,7 +69,7 @@ function rndPts() {
     points.push(pt)
 
     //viz in three
-    const icoGeo = new THREE.IcosahedronGeometry(25)
+    const icoGeo = new THREE.IcosahedronGeometry(5)
     const icoMat = new THREE.MeshNormalMaterial()
     const ico = new THREE.Mesh( icoGeo, icoMat )
     ico.name = 'ico'
@@ -93,6 +109,85 @@ function onChange() {
 
   controls.enabled = false
 
+}
+/**
+ * Gets <input> elements from html and sets handlers
+ * (html is generated from the grasshopper definition)
+ */
+ function getInputs() {
+  const inputs = {}
+  for (const input of document.getElementsByTagName('input')) {
+    switch (input.type) {
+      case 'number':
+        inputs[input.id] = input.valueAsNumber
+        input.onchange = onSliderChange
+        break
+      case 'range':
+        inputs[input.id] = input.valueAsNumber
+        input.onmouseup = onSliderChange
+        input.ontouchend = onSliderChange
+        break
+      case 'checkbox':
+        inputs[input.id] = input.checked
+        input.onclick = onSliderChange
+        break
+      default:
+        break
+    }
+  }
+  return inputs
+}
+
+// more globals
+let scene, camera, renderer, controls
+
+/**
+ * Sets up the scene, camera, renderer, lights and controls and starts the animation
+ */
+function init() {
+
+  // Rhino models are z-up, so set this as the default
+  THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 );
+
+  // create a scene and a camera
+  scene = new THREE.Scene()
+  scene.background = new THREE.Color(1, 1, 1)
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
+  camera.position.set(1, -1, 1) // like perspective view
+
+  // very light grey for background, like rhino
+  scene.background = new THREE.Color('whitesmoke')
+
+  // create the renderer and add it to the html
+  renderer = new THREE.WebGLRenderer({ antialias: true })
+  renderer.setPixelRatio( window.devicePixelRatio )
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
+
+  // add some controls to orbit the camera
+  controls = new OrbitControls(camera, renderer.domElement)
+
+  // add a directional light
+  const directionalLight = new THREE.DirectionalLight( 0xffffff )
+  directionalLight.intensity = 2
+  scene.add( directionalLight )
+
+  const ambientLight = new THREE.AmbientLight()
+  scene.add( ambientLight )
+
+  // handle changes in the window size
+  window.addEventListener( 'resize', onWindowResize, false )
+
+  animate()
+}
+
+function loadContext() {
+loader.load('Public_Space.3dm', function (object) {
+  object.traverse(child => {
+    child.name = 'Public_Space'
+  })
+  scene.add(object)
+})
 }
 
 //calling download function
@@ -174,12 +269,32 @@ function collectResults(responseJson) {
         for( let j = 0; j < branch.length; j ++) {
           // ...load rhino geometry into doc
           const rhinoObject = decodeItem(branch[j])
+    
+          //GET VALUES
+          if (values[i].ParamName == "RH_OUT:area") {
+            //area = JSON.parse(responseJson.values[i].InnerTree['{ 0; }'][0].data)
+            area = Math.round(branch[j].data)
+
+            console.log(area)
+          }
+          //console.log(area)
+
           if (rhinoObject !== null) {
             doc.objects().add(rhinoObject, null)
           }
         }
       }
     }
+
+    if (doc.objects().count < 1) {
+      console.error('No rhino objects to load!')
+      showSpinner(false)
+      return
+    }
+     //GET VALUES
+     document.getElementById('area').innerText = "// PANEL AREA = " + area + " m2"
+
+
 
     if (doc.objects().count < 1) {
       console.error('No rhino objects to load!')
@@ -250,6 +365,33 @@ function decodeItem(item) {
     return rhino.CommonObject.decode(data)
   }
   return null
+}
+
+/**
+ * Called when a slider value changes in the UI. Collect all of the
+ * slider values and call compute to solve for a new scene
+ */
+ function onSliderChange () {
+  showSpinner(true)
+  // get slider values
+  let inputs = {}
+  for (const input of document.getElementsByTagName('input')) {
+    switch (input.type) {
+    case 'number':
+      inputs[input.id] = input.valueAsNumber
+      break
+    case 'range':
+      inputs[input.id] = input.valueAsNumber
+      break
+    case 'checkbox':
+      inputs[input.id] = input.checked
+      break
+    }
+  }
+  
+  data.inputs = inputs
+
+  compute()
 }
 
 /**
