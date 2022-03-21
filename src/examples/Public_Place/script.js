b8914b3f08958531c557e034bf12edbe31c1e08b
maryamd <93530294+maryamagd@users.noreply.github.com>
2022-03-21 03:45:37 +0300

    Edit2

--- src/examples/Public_Place/script.js
+++ src/examples/Public_Place/script.js
@@ -9,10 +9,8 @@ const loader = new Rhino3dmLoader()
 loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/' )
 
 // initialise 'data' object that will be used by compute()
-const data = {
-  definition: 'Canopy_maker.gh',
-  inputs: getInputs()
-}
+const definition = 'Canopy_maker.gh'
+
 // setup input change events
 const Column_Radius_slider = document.getElementById( 'Column_Radius' )
 Column_Radius_slider.addEventListener( 'mouseup', onSliderChange, false )
@@ -252,33 +250,6 @@ function decodeItem(item) {
     return rhino.CommonObject.decode(data)
   }
   return null
-}
-
-/**
- * Called when a slider value changes in the UI. Collect all of the
- * slider values and call compute to solve for a new scene
- */
-function onSliderChange () {
-  showSpinner(true)
-  // get slider values
-  let inputs = {}
-  for (const input of document.getElementsByTagName('input')) {
-    switch (input.type) {
-    case 'number':
-      inputs[input.id] = input.valueAsNumber
-      break
-    case 'range':
-      inputs[input.id] = input.valueAsNumber
-      break
-    case 'checkbox':
-      inputs[input.id] = input.checked
-      break
-    }
-  }
-  
-  data.inputs = inputs
-
-  compute()
 }
 
 /**
